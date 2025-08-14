fdabb98ecca4be42556168d05577570f4a96dae6
jaa88 <1170271773@qq.com>
2025-08-14 09:23:47 +0800

    项目详情页编辑

--- src/views/projectmanage/projectbase/index.vue
+++ src/views/projectmanage/projectbase/index.vue
@@ -116,60 +116,120 @@
     </div>
 
     <!-- 添加或修改岗位对话框 -->
-    <el-dialog id="ffff" :title="addOrUpdateProjectBaseDialogTitle" :visible.sync="addOrUpdateProjectBaseVisible" width="500px" append-to-body>
-      <el-form ref="addOrUpdateProjectBaseForm" :model="addOrUpdateProjectBaseForm" :rules="addOrUpdateProjectBaseFormRules" label-width="80px">
-        <el-form-item label="项目名称" prop="projectName">
-          <el-input v-model="addOrUpdateProjectBaseForm.projectName" placeholder="项目名称" />
-        </el-form-item>
+    <el-dialog id="ffff" :title="addOrUpdateProjectBaseDialogTitle" :visible.sync="addOrUpdateProjectBaseVisible" width="1200px" append-to-body  @close="cancelAddOrUpdateProjectBase">
+      <div style="padding-top: 20px">
+        <el-form ref="addOrUpdateProjectBaseForm" :model="addOrUpdateProjectBaseForm" :rules="addOrUpdateProjectBaseFormRules" label-width="120px">
+          <el-row>
+            <el-col :span="12">
+              <div style="padding-right: 20px">
+                <el-form-item label="项目名称" prop="projectName">
+                  <el-input v-model="addOrUpdateProjectBaseForm.projectName" placeholder="项目名称" />
+                </el-form-item>
 
-        <el-form-item label="相关部门" prop="projectName">
-          <el-select v-model="addOrUpdateProjectBaseForm.canEditProjectDeptIdList" filterable multiple placeholder="请选择">
-            <el-option
-              v-for="item in allDeptList"
-              :key="item.deptId"
-              :label="item.deptName"
-              :value="item.deptId">
-            </el-option>
-          </el-select>
-        </el-form-item>
+                <el-form-item label="项目代码" prop="xiangMuDaiMa">
+                  <el-input v-model="addOrUpdateProjectBaseForm.xiangMuDaiMa" placeholder="项目投资代码" />
+                </el-form-item>
 
-        <el-form-item label="备注" prop="remark">
-          <el-input v-model="addOrUpdateProjectBaseForm.remark" placeholder="备注" />
-        </el-form-item>
+                <el-form-item label="相关部门" prop="projectName">
+                  <el-select v-model="addOrUpdateProjectBaseForm.canEditProjectDeptIdList" filterable multiple placeholder="请选择">
+                    <el-option
+                      v-for="item in allDeptList"
+                      :key="item.deptId"
+                      :label="item.deptName"
+                      :value="item.deptId">
+                    </el-option>
+                  </el-select>
+                </el-form-item>
 
-        <el-form-item label="目录配置" prop="remark">
-          <el-input v-model="addOrUpdateProjectBaseForm.contentsSetStr" placeholder="类似如下“1:前期,2:中期,3:后期”，英文符号" />
-        </el-form-item>
+                <el-form-item label="项目主图" style="margin-bottom: 10px !important;margin-top: 30px !important;">
+                  <el-upload style="" list-type="picture-card"
+                             :file-list="addOrUpdateProjectBaseForm.fileList"
+                             :limit="1"
+                             :on-success="onSuccess" :on-error="onError" :on-remove="handleRemove"
+                             :action="uploadUrl" :headers="headers" accept=".jpg,.jpeg,.png"
+                             :on-preview="handlePictureCardPreview"
+                  >
 
-        <el-form-item label="上传文件" style="margin-bottom: 10px !important;">
-          <el-upload style="" list-type="picture-card"
-                     :file-list="addOrUpdateProjectBaseForm.fileList"
-                     :limit="1"
-                     :on-success="onSuccess" :on-error="onError" :on-remove="handleRemove"
-                     :action="uploadUrl" :headers="headers" accept=".jpg,.jpeg,.png"
-                     :on-preview="handlePictureCardPreview"
-          >
+                       <el-button size="small" type="primary">导入</el-button>
+                  </el-upload>
+                </el-form-item>
 
+                <el-form-item label="项目地址" prop="zongTouZi">
+                  <el-input type="textarea" :rows="3" v-model="addOrUpdateProjectBaseForm.xiangMuDiZhi" placeholder="项目地址" />
+                </el-form-item>
+              </div>
+            </el-col>
 
-               <el-button size="small" type="primary">导入</el-button>
-          </el-upload>
-        </el-form-item>
+            <el-col :span="12">
+              <div style="padding-right: 20px">
+                <el-form-item label="项目类型" prop="xiangMuLeiXing">
+                  <el-input v-model="addOrUpdateProjectBaseForm.xiangMuLeiXing" placeholder="项目类型" />
+                </el-form-item>
 
-        <el-form-item label="初始模板" prop="projectName">
-          <el-select v-model="addOrUpdateProjectBaseForm.cellsJsonStr" filterable placeholder="请选择">
-            <el-option
-              v-for="item in allTemplateList"
-              :key="item.id"
-              :label="item.templateName"
-              :value="item.cellsJsonStr">
-            </el-option>
-          </el-select>
-        </el-form-item>
+                <el-form-item label="建设性质" prop="jianSheXingZhi">
+                  <el-input v-model="addOrUpdateProjectBaseForm.jianSheXingZhi" placeholder="建设性质" />
+                </el-form-item>
 
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="submitForm">确 定</el-button>
-        <el-button @click="cancelAddOrUpdateProjectBase">取 消</el-button>
+                <el-form-item label="总投资" prop="zongTouZi">
+                  <el-input v-model="addOrUpdateProjectBaseForm.zongTouZi" placeholder="总投资" />
+                </el-form-item>
+
+                <el-form-item label="拟开工日期" prop="zongTouZi">
+                  <el-input v-model="addOrUpdateProjectBaseForm.niKaiGongRiQi" placeholder="拟开工日期" />
+                </el-form-item>
+
+                <el-form-item label="拟完工日期" prop="zongTouZi">
+                  <el-input v-model="addOrUpdateProjectBaseForm.niWanGongRiQi" placeholder="拟完工日期" />
+                </el-form-item>
+
+                <el-form-item label="建设单位" prop="projectName">
+                  <el-input v-model="addOrUpdateProjectBaseForm.jianSheDanWei" placeholder="建设单位" />
+                </el-form-item>
+
+                <el-form-item label="项目负责人" prop="projectName">
+                  <el-input v-model="addOrUpdateProjectBaseForm.xiangMuFuZeRen" placeholder="项目负责人" />
+                </el-form-item>
+
+                <el-form-item label="联系方式" prop="projectName">
+                  <el-input v-model="addOrUpdateProjectBaseForm.lianXiFangShi" placeholder="联系方式" />
+                </el-form-item>
+
+              </div>
+            </el-col>
+          </el-row>
+
+          <el-form-item label="主要建设内容" prop="zongTouZi">
+            <el-input type="textarea" :rows="3" v-model="addOrUpdateProjectBaseForm.zhuYaoJianSheNeiRong" placeholder="主要建设内容" />
+          </el-form-item>
+
+          <el-form-item label="拟新增用地情况" prop="zongTouZi">
+            <el-input type="textarea" :rows="3"  v-model="addOrUpdateProjectBaseForm.niXinZenYongDiQingKuang" placeholder="拟新增用地情况" />
+          </el-form-item>
+
+          <el-form-item label="备注" prop="remark">
+            <el-input type="textarea" :rows="3"  v-model="addOrUpdateProjectBaseForm.remark" placeholder="备注" />
+          </el-form-item>
+
+          <el-form-item label="目录配置" prop="remark">
+            <el-input type="textarea" :rows="3"  v-model="addOrUpdateProjectBaseForm.contentsSetStr" placeholder="类似如下“1:前期,2:中期,3:后期”，英文符号" />
+          </el-form-item>
+
+          <el-form-item v-if="addOrUpdateProjectBaseDialogTitle=='新增项目'" label="初始模板" prop="projectName">
+            <el-select v-model="addOrUpdateProjectBaseForm.cellsJsonStr" filterable placeholder="请选择">
+              <el-option
+                v-for="item in allTemplateList"
+                :key="item.id"
+                :label="item.templateName"
+                :value="item.cellsJsonStr">
+              </el-option>
+            </el-select>
+          </el-form-item>
+
+        </el-form>
+        <div slot="footer" class="dialog-footer" style="text-align: right">
+          <el-button type="primary" @click="submitForm">确 定</el-button>
+          <el-button @click="cancelAddOrUpdateProjectBase">取 消</el-button>
+        </div>
       </div>
     </el-dialog>
 
@@ -390,12 +450,24 @@ export default {
         "id":row.id,
         fileList:row.projectMainImagePathAndName!=null && row.projectMainImagePathAndName!=''?[{"projectMainImagePathAndName":row.projectMainImagePathAndName,"url":process.env.VUE_APP_BASE_API+row.projectMainImagePathAndName}]:[],
         projectMainImagePathAndName:row.projectMainImagePathAndName,
-        returnUploadFileList:[]
+        returnUploadFileList:[],
+        //补充
+        xiangMuDaiMa:row.xiangMuDaiMa,
+        xiangMuDiZhi:row.xiangMuDiZhi,
+        xiangMuLeiXing:row.xiangMuLeiXing,
+        jianSheXingZhi:row.jianSheXingZhi,
+        zongTouZi:row.zongTouZi,
+        niKaiGongRiQi:row.niKaiGongRiQi,
+        niWanGongRiQi:row.niWanGongRiQi,
+        jianSheDanWei:row.jianSheDanWei,
+        xiangMuFuZeRen:row.xiangMuFuZeRen,
+        lianXiFangShi:row.lianXiFangShi,
+        zhuYaoJianSheNeiRong:row.zhuYaoJianSheNeiRong,
+        niXinZenYongDiQingKuang:row.niXinZenYongDiQingKuang,
       }
       this.addOrUpdateProjectBaseDialogTitle="修改项目";
       this.addOrUpdateProjectBaseVisible = true;
     },
-
 
     /** 提交按钮 */
     submitForm: function() {
